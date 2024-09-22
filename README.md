901b2cf3bafa8c799dcb6a294f8013c5537b3be2
Jorge Vasquez <jorgefernandov10@gmail.com>
2024-09-21 18:45:13 -0600

    tp2 testing de software

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 Este repositorio muestra la implementación de la metodología de Test Driven Development en un ejemplo simple: El desarrollo de un contolador de 16 leds mapeados en memoria. El ejemplo es una adaptación a ceedling de ejemplo desarrollado en el libro Pragmatic Test Driven Development for Embedded C de James W. Genning
 
 ## Requisitos
-Maneja 16 LEDs de dos estados (encendido y apagado).
-Se puede cambiar el estado de un LED sin afectar a los otros.
-Se puede cambiar el estado de todos los LEDs en una sola operación.
-Se puede recuperar el estado actual de un LED.
-Al iniciar el procesador no hay estado conocido de los LEDs, estos deben ser apagados por software.
-Los LEDs estan mapeados en una palabra de 16 bits, en memoria, en una dirección a determinar.
-Para encender el led se debe escribir un “1” en el bit y para apagarlo se debe * escribir un “0”.
-El led 1 corresponde al bit menos significativo y el led 16 al bit mas significativo.
+Test que se desarrollaron
+```
+Test 1: AL arrancar el sistema, todos los leds se quedan apagados
+Test 2: Prender un LED individual
+Test 3: Prender y apagar un LED individual
+Test 4: Prender y apagar múltiples LED’s
+Test 5: Prender todos los LEDs de una vez
+Test 6: Apagar todos los LEDs de una vez
+Test 7: Consultar el estado de un LED que está encendido
+Test 8: Consultar el estado de un LED que esta apagado
+```
--- test/test_leds.c
+++ test/test_leds.c
@@ -1,49 +1,60 @@
 #include "unity.h"
 #include "leds.h"
-/** @todo
-Prender todos los LEDs de una vez.
-Apagar todos los LEDs de una vez.
-Consultar el estado de un LED que está encendido
-Consultar el estado de un LED que esta apagado
-Revisar limites de los parametros.
-Revisar parámetros fuera de los limites.
-*/
 
 static uint16_t puerto_virtual;
 
-//se ejecuta antes de cada prueba
+/**
+ * @brief Funcion que se ejecuta antes de cada prueba
+ *
+ */
 void setUp(void){
     LedsCreate(&puerto_virtual);
 }
 
-// se ejecuta despues de cada prueba
+
+/**
+ * @brief Funcion que se ejecuta despues de cada prueba
+ *
+ */
 void tearDown(void){
 
 }
 
-// Con la inicializacion todos los leds se quedan apagados
+/**
+ * @brief Test 1: AL arrancar el sistema, todos los leds se quedan apagados
+ *
+ */
 void test_todos_los_leds_inician_apagados(void){
     uint16_t puerto_virtual = 0xFFFF;
     
     LedsCreate(&puerto_virtual);
-    TEST_ASSERT_EQUAL_HEX16(0, puerto_virtual);
+    TEST_ASSERT_EQUAL_HEX16(0x0000, puerto_virtual);
 }
 
-// Prender un LED individual.
+/**
+ * @brief Test 2: Prender un LED individual
+ *
+ */
 void test_prender_un_led_individual(void){
     LedsSetOn(3);
     TEST_ASSERT_EQUAL_HEX16(1 << 2, puerto_virtual);
 }
 
-// Apagar un LED individual.
+/**
+ * @brief Test 3: Prender y apagar un LED individual
+ *
+ */
 void test_prender_y_apagar_un_led_individual(void){
     LedsSetOn(3);
     LedsSetOff(3);
 
-    TEST_ASSERT_EQUAL_HEX16(0, puerto_virtual);
+    TEST_ASSERT_EQUAL_HEX16(0x0000, puerto_virtual);
 }
 
-//Prender y apagar múltiples LED’s.
+/**
+ * @brief Test 4: Prender y apagar múltiples LED’s
+ *
+ */
 void test_prender_y_apagar_varios_leds(void){
     LedsSetOn(7);
     LedsSetOn(5);
@@ -51,4 +62,39 @@ void test_prender_y_apagar_varios_leds(void){
     LedsSetOff(7);
 
     TEST_ASSERT_EQUAL_HEX16(1 << 4, puerto_virtual);
+}
+
+/**
+ * @brief Test 5: Prender todos los LEDs de una vez
+ *
+ */
+ void test_prender_todos_los_leds_juntos(void) {
+    LedsSetAllOn();
+    TEST_ASSERT_EQUAL_HEX16(0xFFFF, puerto_virtual);
+}
+
+/**
+ * @brief Test 6: Apagar todos los LEDs de una vez
+ *
+ */
+ void test_apagar_todos_los_leds_juntos(void) {
+    LedsSetAllOff();
+    TEST_ASSERT_EQUAL_HEX16(0x0000, puerto_virtual);
+}
+
+/**
+ * @brief Test 7: Consultar el estado de un LED que está encendido
+ *
+ */
+ void test_consultar_estado_led_encendido(void) {
+    LedsSetOn(10);
+    TEST_ASSERT_TRUE(LedIsOn(10));
+}
+
+/**
+ * @brief Test 8: Consultar el estado de un LED que esta apagado
+ *
+ */
+ void test_consultar_estado_led_apagado(void){
+    TEST_ASSERT_FALSE(LedIsOn(14));
 }
